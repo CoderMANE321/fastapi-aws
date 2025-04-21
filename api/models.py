e3fe5dcc7b266b113ba01aef7403b8ae2bd3c229
CODERMANE321 <deontamwilliams@gmail.com>
2025-04-21 18:22:42 -0400

    new env

--- api/models.py
+++ api/models.py
@@ -9,10 +9,13 @@ load_dotenv()
 
 Base = declarative_base()
 
+
 if os.getenv("DEPLOYMENT_ENVIRONMENT") == 'DEV':
     engine = create_engine(os.getenv("DB_URL"), connect_args={'check_same_thread': False})
 else:
     engine = create_engine(os.getenv("DB_URL"))
+
+#engine = create_engine("postgresql://rootuser:HltW1I5EOZrFnKAxfLud@fastapi-aws-database.cz84u82s4bga.us-east-2.rds.amazonaws.com:5432/postgres")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
